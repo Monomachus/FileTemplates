656f09b9a637c599c19beb7be146e766d5105188
Monomachus <monomachus85@yahoo.com>
2012-03-05 03:07:09 +0200

    First commit

---
+++ src/org/udevi/fancy.php
@@ -0,0 +1,17 @@
+<?php
+<#assign licenseFirst = "/* ">
+<#assign licensePrefix = " * ">
+<#assign licenseLast = " */">
+<#include "../Licenses/license-${project.license}.txt">
+
+namespace ${namespaceDir}
+
+/**
+ * Description of ${name}
+ *
+ * @author ${user}
+ */
+class ${name} ${extends} ${implements} {
+    //put your code here
+}
+?>
